9e1a05a2a5cc94fcf65bd2bb21690ee248e14a02
vinnsel <vinnsel@hotmail.com>
2020-11-30 18:58:59 +0100

    update1

---
+++ level-1.js
@@ -0,0 +1,16 @@
+//LEVEL 1
+// Question 1
+const frukt = 'banan';
+console.log(frukt);
+
+//Question 2
+let person = {
+    key:1,
+    firstname:"Vidar",
+    lastname:"Innselset",
+    age:39,
+    hobbies: ['running', 'cycle', 'skiing'],
+ 
+  }
+  console.log(person.key, person.firstname, person.lastname, person.age, person.hobbies);
+
---
+++ level-2.js
@@ -0,0 +1,2 @@
+const frukt = 'Banan';
+console.log('Hello World!');
